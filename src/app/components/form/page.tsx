e86522dddf2bf7ee7053bd17bf3b40bb631479ab
kvrivas854 <kellyv.rivas@gmail.com>
2024-09-29 17:36:07 -0400

    commenting this out since it's unused

---
+++ src/app/components/form/page.tsx
@@ -0,0 +1,15 @@
+// import React, { useState } from 'react'
+
+// export default function Form()
+// {
+//     const [formState, setFormState] = useState({});
+//     const handleChange = ({ target }) =>
+//     {
+//         const { name, value } = target;
+//     }
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
